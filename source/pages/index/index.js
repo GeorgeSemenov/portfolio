30e64c16f98765ab71d447d2faf78a613e9c1219
George Semenov <zhora.sem@yandex.ru>
2021-07-04 21:43:26 +0300

    let's do it

---
+++ source/pages/index/index.js
@@ -0,0 +1,4 @@
+import './index.scss';
+import '../../JS-components/libs.js';
+import '../../vendor/nice-select/jquery-nice-select-1.1.0/js/jquery.nice-select.min.js';
+import '../../blocks/input/input.js'
