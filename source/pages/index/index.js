9d3a90b0ad426ac04ccdc7f8927fae5f7c7bc4b5
GeorgeSemenov <zhora.sem@yandex.ru>
2021-07-08 16:18:45 +0300

    before lesson

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,4 +1,3 @@
 import './index.scss';
 import '../../JS-components/libs.js';
 import '../../vendor/nice-select/jquery-nice-select-1.1.0/js/jquery.nice-select.min.js';
-import '../../blocks/input/input.js'
