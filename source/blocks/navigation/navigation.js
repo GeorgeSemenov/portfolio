8b9550c07af636d1289ea1b5dcb739e58f889809
GeorgeSemenov <zhora.sem@yandex.ru>
2021-07-20 16:25:30 +0300

    about-me screen is begin

--- source/blocks/navigation/navigation.js
+++ source/blocks/navigation/navigation.js
@@ -1,2 +1,15 @@
 $(document).ready(function(){
+  let timeForFading = 200;
+  $( ".navigation__item" ).click(function(){
+    if(!$(this).hasClass('active')){
+      let target = $(this).data("target");
+      let previousTarget = $('.navigation__item.active').data("target");
+      
+      $('.navigation__item.active').removeClass('active');
+      $(this).addClass('active');
+      $(`div#${previousTarget}`).fadeOut(timeForFading, ()=>{
+        $(`div#${target}`).fadeIn(timeForFading);
+      });
+    }
+  })
 })
--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,3 +1,4 @@
 import './index.scss';
 import '../../JS-components/libs.js';
 import '../../vendor/nice-select/jquery-nice-select-1.1.0/js/jquery.nice-select.min.js';
+import '../../blocks/navigation/navigation.js';
