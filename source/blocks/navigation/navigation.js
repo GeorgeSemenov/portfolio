0dfcd72c91d7e542b3f277e6ea5340d5e930b11d
GeorgeSemenov <zhora.sem@yandex.ru>
2021-07-21 14:58:54 +0300

    about-me-screen is ready

--- source/blocks/navigation/navigation.js
+++ source/blocks/navigation/navigation.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
   let timeForFading = 200;
+  //Убираем display с помощью js, чтобы fadein запомнил оригинальное значение disple:flex, а не block
+  $("main>div[class*='-screen']").css({"display": 'none'})
+  //Находим тот блок, который должен быть видимым изначально и делаем его таковым
+  $("main>div[data-visible='true']").css({"display": 'flex'})
+  
   $( ".navigation__item" ).click(function(){
     if(!$(this).hasClass('active')){
       let target = $(this).data("target");
@@ -8,7 +13,8 @@ $(document).ready(function(){
       $('.navigation__item.active').removeClass('active');
       $(this).addClass('active');
       $(`div#${previousTarget}`).fadeOut(timeForFading, ()=>{
-        $(`div#${target}`).fadeIn(timeForFading);
+        $(`div#${target}`).fadeIn(timeForFading, ()=>{
+        });
       });
     }
   })
