fbbc448c81312e929a5e44ca90adf5a42acfe574
GeorgeSemenov <zhora.sem@yandex.ru>
2021-07-28 16:18:56 +0300

    backgroun is already now

--- source/blocks/portfolio-screen/portfolio-screen.js
+++ source/blocks/portfolio-screen/portfolio-screen.js
@@ -1,2 +1,31 @@
 $(document).ready(function(){
+  elements = $(".portfolio-screen__example-container");
+  for(i = 0; i<= elements.length; i++){
+    let element = elements[i] ;
+    let square = element.getBoundingClientRect();
+    let centerX = square.width/2;
+    let centerY = square.height/2;
+    element.onmouseenter = ()=>{handler(square,{x:centerX,y:centerY},true)}
+  }
 })
+
+function handler(square,center,isMouseMovingFromOutside=true){
+  let event = window.event; 
+  let dx = event.pageX - square.left;//из координаты возбудителя события (мыши) вычетаем координату блока - получаем дэльтаX
+  let dy = event.pageY - square.top;
+  awaytost($(this));
+  if( Math.abs(center.x - dx) > Math.abs(center.y - dy)){
+    if(dx > center.x){console.log(`right`)}
+    else{console.log(`left`);}
+  }else{
+    if(dy > center.y){console.log(`bottom`)}
+    else{console.log(`top`)}
+  }
+}
+
+function takeOver(node,direction="left",isMouseMovingFromOutside=true){
+  // node
+}
+function awaytost(node){
+  $(node).css({"border":"10px solid red"});
+}
--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -2,3 +2,4 @@ import './index.scss';
 import '../../JS-components/libs.js';
 import '../../vendor/nice-select/jquery-nice-select-1.1.0/js/jquery.nice-select.min.js';
 import '../../blocks/navigation/navigation.js';
+import '../../blocks/portfolio-screen/portfolio-screen.js';
