a678efc218a270ead0790ee7e6e7d692a654bafe
GeorgeSemenov <zhora.sem@yandex.ru>
2021-07-30 16:18:33 +0300

    eh ti

--- source/blocks/portfolio-screen/portfolio-screen.js
+++ source/blocks/portfolio-screen/portfolio-screen.js
@@ -5,27 +5,83 @@ $(document).ready(function(){
     let square = element.getBoundingClientRect();
     let centerX = square.width/2;
     let centerY = square.height/2;
-    element.onmouseenter = ()=>{handler(square,{x:centerX,y:centerY},true)}
+    element.onmouseenter = ()=>{handler(element,square,{x:centerX,y:centerY},true)}
+    element.onmouseleave = ()=>{handler(element,square,{x:centerX,y:centerY},false)}
   }
 })
 
-function handler(square,center,isMouseMovingFromOutside=true){
+function handler(node,square,center,isMouseMovingFromOutside=true){
   let event = window.event; 
   let dx = event.pageX - square.left;//из координаты возбудителя события (мыши) вычетаем координату блока - получаем дэльтаX
   let dy = event.pageY - square.top;
-  awaytost($(this));
   if( Math.abs(center.x - dx) > Math.abs(center.y - dy)){
-    if(dx > center.x){console.log(`right`)}
-    else{console.log(`left`);}
+    if(dx > center.x){
+      console.log(`right side`);
+      isMouseMovingFromOutside ? moveFromRightSide(node) : moveToRightSide(node);
+    }
+    else{
+      console.log(`left side`);
+      isMouseMovingFromOutside ? moveFromLeftSide(node): moveToLeftSide(node);
+    }
   }else{
-    if(dy > center.y){console.log(`bottom`)}
-    else{console.log(`top`)}
+    if(dy > center.y){
+      console.log(`bottom side`);
+      isMouseMovingFromOutside ? moveFromBottomSide(node) : moveToBottomSide(node);
+    }
+    else{
+      console.log(`top side`);
+      isMouseMovingFromOutside ? moveFromTopSide(node) : moveToTopSide(node);
+    }
   }
 }
 
-function takeOver(node,direction="left",isMouseMovingFromOutside=true){
-  // node
+//Увы, композиция типа node.css({direction: "0"}) - не работает, т.к. переменная direction должна быть изначально определенна. 
+//поэтому приходится всё явно указывать, очень неудобно
+function moveFromLeftSide(node){
+  $(node).children(".portfolio-screen__example-hrefs")
+             .css({"right":'100%',"bottom":"0"});
+  setTimeout(()=>{
+    $(node).children(".portfolio-screen__example-hrefs")
+          .css({'right':'0'});
+  },10)
 }
-function awaytost(node){
-  $(node).css({"border":"10px solid red"});
+function moveToLeftSide(node){
+  $(node).children(".portfolio-screen__example-hrefs")
+             .css({"right":'100%',"bottom":"0"});
+}
+function moveFromRightSide(node){
+  $(node).children(".portfolio-screen__example-hrefs")
+             .css({"right":'-100%'});
+  setTimeout(()=>{
+    $(node).children(".portfolio-screen__example-hrefs")
+          .css({'right':'0'});
+  },10)
+}
+function moveToRightSide(node){
+  $(node).children(".portfolio-screen__example-hrefs")
+             .css({"right":'-100%'});
+}
+function moveFromBottomSide(node){
+  $(node).children(".portfolio-screen__example-hrefs")
+             .css({"bottom":'-100%'});
+  setTimeout(()=>{
+    $(node).children(".portfolio-screen__example-hrefs")
+          .css({'bottom':'0'});
+  },10)
+}
+function moveToBottomSide(node){
+  $(node).children(".portfolio-screen__example-hrefs")
+             .css({"bottom":'-100%'});
+}
+function moveFromTopSide(node){
+  $(node).children(".portfolio-screen__example-hrefs")
+             .css({"bottom":'100%'});
+  setTimeout(()=>{
+    $(node).children(".portfolio-screen__example-hrefs")
+          .css({'bottom':'0'});
+  },10)
+}
+function moveToTopSide(node){
+  $(node).children(".portfolio-screen__example-hrefs")
+             .css({"bottom":'100%'});
 }
