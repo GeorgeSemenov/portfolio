98a0a957ec05c926b4fe6ea9498d120811c4d936
George Semenov <zhora.sem@yandex.ru>
2021-08-20 17:46:01 +0300

    before lesson

--- acopu.sh
+++ acopu.sh
@@ -3,6 +3,6 @@ read -p "enter your commit message: " messagues
 echo $messagues
 git add --all
 git commit -m "${messagues}"
-git push 
+git push origin
 #Для его работоспособности, пришлось перевести репозиторий с https на ssh
 #Чтобы при push он не запрашивал логин и пароль
